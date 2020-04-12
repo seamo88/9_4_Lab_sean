abce184bb69ee28a582367d5c649cf4b3ed2225d
seamo88 <bstockgame.88@gmail.com>
2020-04-12 09:10:47 -0500

    Add project files.

---
+++ 9_4/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace _9_4HTML
+{
+    class Header
+    {
+        public const string open = "<h1>";
+        public const string close = "</h1>";
+
+        public string CreateHeader(string text)
+        {
+            string header = String.Concat(open, text, close, "\n");
+            return header;
+        }
+    }
+
+
+}
+
+class UnorderedList
+{
+    public const string open = "<ul>";
+    public const string close = "</ul>";
+
+    public string CreateListItem(string text)
+    {
+        string open = "<li>\n";
+        string close = "</li>\n";
+
+        string listItem = String.Concat(open, text, close, "\n");
+        return listItem;
+    }
+
+    public StringBuilder CreateList(string[] listItems)
+    {
+
+        StringBuilder sb = new StringBuilder();
+        sb.Append(open);
+        foreach (string item in listItems)
+        {
+            sb.Append(item);
+        }
+        sb.Append(close);
+
+        return sb;
+    }
+
+}
+
